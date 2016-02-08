301c4b559c73a3df4da3dd18d40d0ce2269b3134
John Robison <10904512@live.mercer.edu>
2016-02-08 03:40:13 -0500

    Added tests for various number sequences.

---
+++ MathLibrary/MathLibrary/NumberSequence.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    public class NumberSequence
+    {
+        public static uint Fibonacci(uint position)
+        {
+            return 0;
+        }
+
+        public static uint Catalan(uint position)
+        {
+            return 0;
+        }
+
+        public static uint Bell(uint position)
+        {
+            return 0;
+        }
+    }
+}
--- MathLibrary/MathLibraryTests/UnitTest1.cs
+++ MathLibrary/MathLibraryTests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathLibrary;
 
 namespace MathLibraryTests
 {
@@ -7,8 +8,36 @@ namespace MathLibraryTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FibonacciTest()
         {
+            Assert.AreEqual(0, NumberSequence.Fibonacci(0));
+            Assert.AreEqual(1, NumberSequence.Fibonacci(1));
+            Assert.AreEqual(1, NumberSequence.Fibonacci(2));
+            Assert.AreEqual(5, NumberSequence.Fibonacci(5));
+            Assert.AreEqual(55, NumberSequence.Fibonacci(10));
+            Assert.AreEqual(6765, NumberSequence.Fibonacci(20));
+        }
+
+        [TestMethod]
+        public void CatalanTest()
+        {
+            Assert.AreEqual(1, NumberSequence.Catalan(0));
+            Assert.AreEqual(1, NumberSequence.Catalan(1));
+            Assert.AreEqual(2, NumberSequence.Catalan(2));
+            Assert.AreEqual(42, NumberSequence.Catalan(5));
+            Assert.AreEqual(16796, NumberSequence.Catalan(10));
+            Assert.AreEqual(6564120420, NumberSequence.Catalan(20));
+        }
+
+        [TestMethod]
+        public void BellTest()
+        {
+            Assert.AreEqual(1, NumberSequence.Bell(0));
+            Assert.AreEqual(1, NumberSequence.Bell(1));
+            Assert.AreEqual(2, NumberSequence.Bell(2));
+            Assert.AreEqual(52, NumberSequence.Bell(5));
+            Assert.AreEqual(11597, NumberSequence.Bell(10));
+            Assert.AreEqual(5172415823572, NumberSequence.Bell(20));
         }
     }
 }
